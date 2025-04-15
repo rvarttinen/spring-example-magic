478f352c3c83bda74e44bcd285876ceaadca4376
rvarttinen <robert.varttinen@autocorrect.se>
2025-04-15 08:33:03 +0200

    Added package description.

---
+++ src/main/java/se/autocorrect/springexample/util/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package harbors a collection of ¨some useful utilities.
+ * <p>
+ * Please keep in mind thar these utiulites may be moved to a repository of its
+ * own. A separate JAR will be deployed as a result.
+ * </p>
+ */
+package se.autocorrect.springexample.util;
