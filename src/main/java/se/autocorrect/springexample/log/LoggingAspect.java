e3cd0e08d630d8ceb66e051bb2b41b43910ecb73
Robert Varttinen <robert.varttinen@autocorrect.se>
2025-05-13 14:01:58 +0200

    Replaced log-message concatenation with message formatting.

--- src/main/java/se/autocorrect/springexample/log/LoggingAspect.java
+++ src/main/java/se/autocorrect/springexample/log/LoggingAspect.java
@@ -41,34 +41,34 @@ public class LoggingAspect {
 	public void logBeforeServiceMethod(JoinPoint joinPoint) {
 		
 		Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
-		logger.info("Entering method: " + joinPoint.getSignature());
+        logger.info("Entering method: {}", joinPoint.getSignature());
 	}
 
 	@AfterReturning(pointcut = "execution(* se.autocorrect.springexample.services.*.*(..))", returning = "result")
 	public void logAfterServiceMethod(JoinPoint joinPoint, Object result) {
 		
 		Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
-		logger.info("Method " + joinPoint.getSignature().getName() + " executed with result: " + result);
+        logger.info("Method {} executed with result: {}", joinPoint.getSignature().getName(), result);
 	}
 	
 	@Before("execution(* se.autocorrect.springexample.infrastructure.*.*(..))")
 	public void logBeforeInfraMethod(JoinPoint joinPoint) {
 		
 		Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
-		logger.info("Entering method: " + joinPoint.getSignature());
+        logger.info("Entering method: {}", joinPoint.getSignature());
 	}
 
 	@AfterReturning(pointcut = "execution(* se.autocorrect.springexample.infrastructure.*.*(..))", returning = "result")
 	public void logAfterInfraMethod(JoinPoint joinPoint, Object result) {
 		
 		Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
-		logger.info("Method " + joinPoint.getSignature().getName() + " executed with result: " + result);
+        logger.info("Method {} executed with result: {}", joinPoint.getSignature().getName(), result);
 	}
 
 	@AfterThrowing(pointcut = "execution(* se.autocorrect.springexample.*.*(..))", throwing = "ex")
 	public void logAfterThrowing(JoinPoint joinPoint, Throwable ex) {
 		
 		Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
-		logger.error("Exception in method: " + joinPoint.getSignature().getName(), ex);
+        logger.error("Exception in method: {}", joinPoint.getSignature().getName(), ex);
 	}
 }
