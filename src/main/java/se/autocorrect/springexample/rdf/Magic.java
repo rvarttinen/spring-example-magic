784a14a1c97c99d0c0f5211202e450a99e976d79
Robert Varttinen <robert.varttinen@autocorrect.se>
2025-05-19 14:20:10 +0200

    Fixed some warnings.

--- src/main/java/se/autocorrect/springexample/rdf/Magic.java
+++ src/main/java/se/autocorrect/springexample/rdf/Magic.java
@@ -25,13 +25,13 @@
  */
 package se.autocorrect.springexample.rdf;
 
-import java.util.Optional;
-
 import org.apache.jena.graph.Node;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.ResourceFactory;
+
+import java.util.Optional;
 
 /**
  * The Magic vocabulary. It reflects the contents of the vocabulary as defined
