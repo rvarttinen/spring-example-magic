2443e73ab572eb441f95e59f37d76143b7548c64
Robert Varttinen <robert.varttinen@autocorrect.se>
2025-05-19 15:07:16 +0200

    Added port extraction util.

---
+++ src/main/java/se/autocorrect/springexample/util/ServerPortExtractor.java
@@ -0,0 +1,30 @@
+package se.autocorrect.springexample.util;
+
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * A simple class to extract the current port set in the web server it starts up.
+ */
+@Service
+public class ServerPortExtractor {
+
+    // TODO: check out: https://www.baeldung.com/spring-boot-running-port
+
+    private int port;
+
+    public int getport() {
+        return port;
+    }
+
+    /**
+     * Await the web server to start spo we can get the current port used, whether fixed by setting or random.
+     *
+     * @param event
+     */
+    @EventListener
+    public void onApplicationEvent(final ServletWebServerInitializedEvent event) {
+        port = event.getWebServer().getPort();
+    }
+}
