2df753229a3dd2fb874f5d2913fcb350c6e40dd0
Robert Varttinen <robert.varttinen@autocorrect.se>
2025-05-22 09:53:02 +0200

    Refactoring; fixed typo in method name, renamed class to avoid another typo and some minor restructuring of the code.

--- src/main/java/se/autocorrect/springexample/api/MagicAsyncController.java
+++ src/main/java/se/autocorrect/springexample/api/MagicAsyncController.java
@@ -1,9 +1,33 @@
+/*-
+ *  
+ * spring-example-magic
+ *  
+ * Copyright (C) 2025 Autocorrect Design HB
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *  
+ */
 package se.autocorrect.springexample.api;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
@@ -18,24 +42,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import se.autocorrect.springexample.rdf.LDMediaType;
 import se.autocorrect.springexample.rdf.LDMediaTypes;
 import se.autocorrect.springexample.services.AsyncService;
+import se.autocorrect.springexample.util.HeaderContentTypeUtil;
 
 @Experimental
 @RestController
 @RequestMapping("/v2")
-public class MagicAsynchController {
+public class MagicAsyncController {
 
 	public static final String NO_KEY = null;
 	
 	private final AsyncService asyncService;
-	private final Executor asyncExecutor;
 
-	MagicAsynchController(@Qualifier("asyncService") AsyncService asyncService, @Qualifier("asyncExecutor") Executor asyncExecutor) {
+    MagicAsyncController(@Qualifier("asyncService") AsyncService asyncService/*, @Qualifier("asyncExecutor") Executor asyncExecutor*/) {
 		this.asyncService = asyncService;
-		this.asyncExecutor = asyncExecutor;
-	}
+    }
 
 	@GetMapping(value = "/magic", produces = { LDMediaTypes.TEXT_TURTLE, LDMediaTypes.RDF_XML, LDMediaTypes.JSON_LD })
 	public ResponseEntity<Model> getMagicAsync(
@@ -51,12 +73,12 @@ public class MagicAsynchController {
 		
 		HttpHeaders headers = calculateLDContentTypeHeader(accept);
 
-		return headers.isEmpty() ? new ResponseEntity<>(HttpStatus.UNSUPPORTED_MEDIA_TYPE) : createOkReponse(union, headers);
+		return headers.isEmpty() ? new ResponseEntity<>(HttpStatus.UNSUPPORTED_MEDIA_TYPE) : createOkResponse(union, headers);
 	}
 
-	private ResponseEntity<Model> createOkReponse(Model union, HttpHeaders headers) {
+	private ResponseEntity<Model> createOkResponse(Model union, HttpHeaders headers) {
 		
-		return union.isEmpty() ? new ResponseEntity<Model>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(union, headers, HttpStatus.OK);
+		return union.isEmpty() ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(union, headers, HttpStatus.OK);
 	}
 
 	private Model combineResults(Model listModel, Model wikiDataModel) {
@@ -70,16 +92,7 @@ public class MagicAsynchController {
 	}
 	
 	private HttpHeaders calculateLDContentTypeHeader(String accept) {
-		
-		var headers = new HttpHeaders();
 
-		switch (accept) {
-
-		case LDMediaTypes.TEXT_TURTLE -> headers.setContentType(LDMediaType.TEXT_TURTLE.asMediaType());
-		case LDMediaTypes.JSON_LD -> headers.setContentType(LDMediaType.JSON_LD.asMediaType());
-		case LDMediaTypes.RDF_XML -> headers.setContentType(LDMediaType.RDF_XML.asMediaType());
-		}
-
-		return headers;
+        return HeaderContentTypeUtil.calculateLDContentTypeHeader(accept);
 	}
 }
