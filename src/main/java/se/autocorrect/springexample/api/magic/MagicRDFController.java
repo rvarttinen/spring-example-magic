89912b47ebcaa1c290b58b751cbfd1a2d906c404
Robert Varttinen <robert.varttinen@autocorrect.se>
2025-05-23 08:59:15 +0200

    Refactored package name to better reflect purpose.

--- src/main/java/se/autocorrect/springexample/api/magic/MagicRDFController.java
+++ src/main/java/se/autocorrect/springexample/api/magic/MagicRDFController.java
@@ -1,22 +1,17 @@
-package se.autocorrect.springexample.api.rdf;
+package se.autocorrect.springexample.api.magic;
 
-import java.util.Optional;
-
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.*;
 import se.autocorrect.springexample.rdf.LDMediaTypes;
 import se.autocorrect.springexample.rdf.Magic;
 import se.autocorrect.springexample.util.HeaderContentTypeUtil;
+
+import java.util.Optional;
 
 /**
  * A specialized controller for serving the {@code Magic}-vocabulary resource
